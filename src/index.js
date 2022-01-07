32e3c7f0b52739f13f8becfb5ee6e5ab12872e25
Tiago Camargo <tiagofcamargo@gmail.com>
2022-01-07 11:42:04 -0300

    JS STACK Project only node without express

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const { URL } = require('url');
+
+const bodyParser = require('./helpers/bodyParser');
+const routes = require('./routes');
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);  
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if(splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === pathname && routeObj.method === request.method
+  ));
+
+  if (route) {
+    request.query = parsedUrl.query;
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application.js'});
+      response.end(JSON.stringify(body))
+    };
+
+    if (['POST', 'PUT', 'PATCH'].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+    
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+  }
+});
+
+server.listen(3000, () => console.log('Sever started ate http://localhost:3000'));
